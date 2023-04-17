797a0302c8a8d9b3f822d67ec8b19f441e7015b2
Nirmita <nirmita.kc@tekkon.com.np>
2023-04-17 16:23:38 +0545

    custom command

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,18 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+
+  defaultCommandTimeout: 19997,
+  responseTimeout: 60042,
+  videoUploadOnPasses: false,
+  viewportWidth: 1200,
+  viewportHeight: 1200,
+
+  env : {
+    ADMIN_USERNAME: "admin@admin.com",
+    ADMIN_PASSWORD: "#t3k@d2021*",
+  },
+
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
